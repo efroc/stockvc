ab308ae1ef7414332c7b31a0d57ef40f0345bb2a
evan <evan.froc@etudiant.univ-rennes1.fr>
2024-06-06 09:17:38 +0200

    re push .sql

--- mySQL/stockvc.sql
+++ mySQL/stockvc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 06 juin 2024 à 09:09
+-- Généré le : jeu. 06 juin 2024 à 09:17
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -34,6 +34,22 @@ CREATE TABLE `historique` (
   `action` varchar(40) NOT NULL,
   `message` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `historique`
+--
+
+INSERT INTO `historique` (`id`, `date`, `reference`, `action`, `message`) VALUES
+(1, '2024-06-06', '1-1', 'Ajout au stock', '1 cable ethernet 1m ont été ajouté au stock.'),
+(2, '2024-06-06', '1-2', 'Ajout au stock', '1 cable ethernet 2m ont été ajouté au stock.'),
+(3, '2024-06-06', '1-5 ', 'Ajout au stock', '1 cable ethernet 5m ont été ajouté au stock.'),
+(4, '2024-06-06', '1-10', 'Ajout au stock', '1 cable ethernet 10m ont été ajouté au stock.'),
+(5, '2024-06-06', '2-5', 'Ajout au stock', '1 cable vga 5m ont été ajouté au stock.'),
+(6, '2024-06-06', '2-10', 'Ajout au stock', '1 cable vga 10m ont été ajouté au stock.'),
+(7, '2024-06-06', '3', 'Ajout au stock', '1 cable alim ont été ajouté au stock.'),
+(8, '2024-06-06', '3-S', 'Ajout au stock', '1 cable alim special ont été ajouté au stock.'),
+(9, '2024-06-06', '4', 'Ajout au stock', '1 cable tripolaire ont été ajouté au stock.'),
+(10, '2024-06-06', '1-05', 'Ajout au stock', '1 cable ethernet 0.5m ont été ajouté au stock.');
 
 -- --------------------------------------------------------
 
@@ -69,16 +85,16 @@ CREATE TABLE `stock` (
 --
 
 INSERT INTO `stock` (`ident`, `reference`, `materiel`, `marque`, `etat`, `note`) VALUES
-(185, '1-1', 'cable ethernet 1m', '', 'disponible', ''),
-(186, '1-2', 'cable ethernet 2m', '', 'disponible', ''),
-(187, '1-5', 'cable ethernet 5m', '', 'disponible', ''),
-(188, '1-10', 'cable ethernet 10m', '', 'disponible', ''),
-(189, '2-5', 'cable vga 5m', '', 'disponible', ''),
-(190, '2-10', 'cable vga 10m', '', 'disponible', ''),
-(191, '3', 'cable alim', '', 'disponible', ''),
-(192, '3-S', 'cable alim special', '', 'disponible', ''),
-(193, '4', 'cable tripolaire', '', 'disponible', ''),
-(210, '1-05', 'cable ethernet 50cm', '', 'disponible', '');
+(1, '1-1', 'cable ethernet 1m', '', 'disponible', ''),
+(2, '1-2', 'cable ethernet 2m', '', 'disponible', ''),
+(3, '1-5 ', 'cable ethernet 5m', '', 'disponible', ''),
+(4, '1-10', 'cable ethernet 10m', '', 'disponible', ''),
+(5, '2-5', 'cable vga 5m', '', 'disponible', ''),
+(6, '2-10', 'cable vga 10m', '', 'disponible', ''),
+(7, '3', 'cable alim', '', 'disponible', ''),
+(8, '3-S', 'cable alim special', '', 'disponible', ''),
+(9, '4', 'cable tripolaire', '', 'disponible', ''),
+(10, '1-05', 'cable ethernet 0.5m', '', 'disponible', '');
 
 --
 -- Index pour les tables déchargées
@@ -110,13 +126,13 @@ ALTER TABLE `stock`
 -- AUTO_INCREMENT pour la table `historique`
 --
 ALTER TABLE `historique`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=460;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `stock`
 --
 ALTER TABLE `stock`
-  MODIFY `ident` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=222;
+  MODIFY `ident` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
